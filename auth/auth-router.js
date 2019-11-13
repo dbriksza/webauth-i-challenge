9111fdc8df658a1422a356370a605726b206f40c
dbriksza <dbriksza@gmail.com>
2019-11-13 16:21:06 -0500

    mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -12,6 +12,7 @@ router.post("/register", (req, res) => {
 
   Users.add(userInformation)
     .then(saved => {
+      // req.session.username = saved.username;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -28,6 +29,7 @@ router.post("/login", (req, res) => {
     .then(user => {
       // check that the password is valid
       if (user && bcrypt.compareSync(password, user.password)) {
+        // req.session.username = user.username;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
@@ -37,6 +39,20 @@ router.post("/login", (req, res) => {
       console.log("login error", error);
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({ message: "you're still here" });
+      } else {
+        res.status(200).json({ message: "logged out successfully" });
+      }
+    });
+  } else {
+    res.status(200).json({ message: "ok bye I guess" });
+  }
 });
 
 module.exports = router;
